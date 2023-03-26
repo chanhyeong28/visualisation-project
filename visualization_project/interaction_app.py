351823a50d4a11c11a96a9a2cfcd529f6773c530
chanhyeong28 <chanhyeong28@snu.ac.kr>
2023-03-26 21:04:37 +0900

    [update] rescale nodesize

--- visualization_project/interaction_app.py
+++ visualization_project/interaction_app.py
@@ -88,8 +88,8 @@ if response.status_code == 200:
         protocols_tvl = {protocol : df.loc[df['protocol'] == protocol, 'protocol_tvl'].unique().tolist() for protocol in protocols}
 
         # Set nodes size
-        chain_size = [np.exp(0.0001 * chains_tvl[chain][0]) for chain in chains]
-        protocol_size = [np.exp(0.0001 * protocols_tvl[protocol][0]) for protocol in protocols]
+        chain_size = [np.exp(0.1 * chains_tvl[chain][0]) for chain in chains]
+        protocol_size = [np.exp(0.01 * protocols_tvl[protocol][0]) for protocol in protocols]
         
         #add chain_nodes
         G.add_nodes_from(chains, value = chain_size)
