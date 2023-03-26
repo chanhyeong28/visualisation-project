33c0ce38eebe13a12fe83c1d3af2846d53714958
chanhyeong28 <chanhyeong28@snu.ac.kr>
2023-03-26 21:46:40 +0900

    check

--- visualization_project/interaction_app.py
+++ visualization_project/interaction_app.py
@@ -95,8 +95,8 @@ if response.status_code == 200:
         protocols_tvl = {protocol : df.loc[df['protocol'] == protocol, 'protocol_tvl'].unique().tolist() for protocol in protocols}
 
         # Set nodes size
-        chain_size = [0.0000000001 * chains_tvl[chain][0] for chain in chains]
-        protocol_size = [0.0000000001 * protocols_tvl[protocol][0] for protocol in protocols]
+        chain_size = [chains_tvl[chain][0] for chain in chains]
+        protocol_size = [protocols_tvl[protocol][0] for protocol in protocols]
         
         #add chain_nodes
         G.add_nodes_from(chains, value = chain_size)
